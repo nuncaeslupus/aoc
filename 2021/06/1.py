0cea90d961f22c1307dabe1d406ab8c0c6d793f7
ivan <imarcos@gmail.com>
2022-12-04 18:12:25 +0100

    2021 06

--- 2021/06/1.py
+++ 2021/06/1.py
@@ -1,8 +1,18 @@
+import numpy as np
+
 data_file = "sample.txt"
-# data_file = "data.txt"
+data_file = "data.txt"
+
+days = 80
 
 with open(data_file) as f:
-    lines = f.read().splitlines()
+    line = f.read().splitlines()[0].split(",")
+    timers = np.array(line, dtype=int)
 
-    for l in lines:
-        pass
+    for day in range(days):
+        timers -= 1
+        negative = timers == -1
+        timers[negative] = 6
+        timers = np.append(timers, np.ones(np.count_nonzero(negative), dtype=int) * 8)
+    result = len(timers)
+    print(result)
--- 2021/06/2.py
+++ 2021/06/2.py
@@ -1,8 +1,25 @@
+import numpy as np
+
 data_file = "sample.txt"
-# data_file = "data.txt"
+data_file = "data.txt"
+
+days = 256
 
 with open(data_file) as f:
-    lines = f.read().splitlines()
+    line = f.read().splitlines()[0].split(",")
+    timers = np.array(line, dtype=int)
 
-    for l in lines:
-        pass
+    values = range(0, 9)
+    unique, counts = np.unique(timers, return_counts=True)
+    counters = np.array([0] * 9)
+    for val in values:
+        pos = np.where(unique == val)[0]
+        if pos.size > 0:
+            counters[val] = counts[pos]
+
+    for day in range(days):
+        negative = counters[0]
+        counters = np.roll(counters, -1)
+        counters[6] += negative
+    result = counters.sum()
+    print(result)
