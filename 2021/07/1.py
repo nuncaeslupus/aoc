3058a26fd9e48e93e42468ed869d9dbb7586866a
ivan <imarcos@gmail.com>
2022-12-04 18:44:11 +0100

    2021 07

--- 2021/07/1.py
+++ 2021/07/1.py
@@ -1,8 +1,20 @@
+import numpy as np
+
 data_file = "sample.txt"
-# data_file = "data.txt"
+data_file = "data.txt"
 
 with open(data_file) as f:
-    lines = f.read().splitlines()
+    line = f.read().splitlines()[0].split(",")
+    positions = np.array(line, dtype=int)
 
-    for l in lines:
-        pass
+    min = np.min(positions)
+    max = np.max(positions)
+
+    min_fuel = 10000000000000000
+    for pos in range(min, max + 1):
+        new_positions = positions - pos
+        fuel = np.abs(new_positions).sum()
+        if fuel < min_fuel:
+            min_fuel = fuel
+
+    print(min_fuel)
--- 2021/07/2.py
+++ 2021/07/2.py
@@ -1,8 +1,20 @@
+import numpy as np
+
 data_file = "sample.txt"
-# data_file = "data.txt"
+data_file = "data.txt"
 
 with open(data_file) as f:
-    lines = f.read().splitlines()
+    line = f.read().splitlines()[0].split(",")
+    positions = np.array(line, dtype=int)
 
-    for l in lines:
-        pass
+    min = np.min(positions)
+    max = np.max(positions)
+
+    min_fuel = np.iinfo(np.int64).max
+    for pos in range(min, max + 1):
+        new_positions = positions - pos
+        fuel = (np.abs(new_positions) * (np.abs(new_positions) + 1) / 2).sum()
+        if fuel < min_fuel:
+            min_fuel = fuel
+
+    print(int(min_fuel))
