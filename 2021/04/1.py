d9e1455bbade9a3a73099eb51be51acce6f8fb97
ivan <imarcos@gmail.com>
2022-12-04 13:11:37 +0100

    2021 04

--- 2021/04/1.py
+++ 2021/04/1.py
@@ -1,8 +1,29 @@
+import numpy as np
+import pandas as pd
+
 data_file = "sample.txt"
-# data_file = "data.txt"
+data_file = "data.txt"
 
 with open(data_file) as f:
     lines = f.read().splitlines()
 
-    for l in lines:
-        pass
+    drawn = np.array(lines[0].split(","), dtype=int)
+    boards: list[pd.DataFrame] = []
+    for i in range(2, len(lines), 6):
+        board = [np.array(row.split(), dtype=int) for row in lines[i : i + 5]]
+        df = pd.DataFrame(board)
+        boards.append(df)
+
+    d = None
+    winner = None
+    for d in drawn:
+        for df in boards:
+            df.mask(df == d, -1, inplace=True)
+            if (df.sum(axis=0) == -5).any() or (df.sum(axis=1) == -5).any():
+                winner = df
+                break
+        if winner is not None:
+            break
+    score = int(winner.replace(-1, np.NaN).sum().sum())
+    result = d * score
+    print(result)
--- 2021/04/2.py
+++ 2021/04/2.py
@@ -1,8 +1,37 @@
+import numpy as np
+import pandas as pd
+
 data_file = "sample.txt"
-# data_file = "data.txt"
+data_file = "data.txt"
 
 with open(data_file) as f:
     lines = f.read().splitlines()
 
-    for l in lines:
-        pass
+    drawn = np.array(lines[0].split(","), dtype=int)
+    boards: list[pd.DataFrame] = []
+    for i in range(2, len(lines), 6):
+        board = [np.array(row.split(), dtype=int) for row in lines[i : i + 5]]
+        df = pd.DataFrame(board)
+        boards.append(df)
+
+    losers = list(range(len(boards)))
+
+    d = None
+    winner = None
+    for d in drawn:
+        for i, df in enumerate(boards):
+            df.mask(df == d, -1, inplace=True)
+            if i in losers and (
+                (df.sum(axis=0) == -5).any() or (df.sum(axis=1) == -5).any()
+            ):
+                if len(losers) == 1:
+                    winner = df
+                    break
+                else:
+                    losers.remove(i)
+        if winner is not None:
+            break
+
+    score = int(winner.replace(-1, np.NaN).sum().sum())
+    result = d * score
+    print(result)
