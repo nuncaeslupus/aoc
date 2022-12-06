f48e65c39b8c01c15768599d7da4fa3e1cee2a89
ivan <imarcos@gmail.com>
2022-12-06 20:50:49 +0100

    2021 Improved template and data acquired

--- 2021/15/1.py
+++ 2021/15/1.py
@@ -1,8 +1,17 @@
+from timeit import default_timer as timer
+
 data_file = "sample.txt"
 # data_file = "data.txt"
 
+start = timer()
 with open(data_file) as f:
     lines = f.read().splitlines()
 
+    result = None
     for l in lines:
         pass
+
+    print(f"Result: {result}")
+
+end = timer()
+print(f"Total time: {end - start:.5f} seconds.")
