92249df22438958960ad7e7b5b01dfd207d39914
ivan <imarcos@gmail.com>
2022-12-10 11:38:48 +0100

    2022 10

--- 2022/10/1.py
+++ 2022/10/1.py
@@ -1,15 +1,28 @@
 from timeit import default_timer as timer
 
 data_file = "sample.txt"
-# data_file = "data.txt"
+data_file = "data.txt"
 
 start = timer()
 with open(data_file) as f:
     lines = f.read().splitlines()
 
-    result = None
+    time = 1
+    reg = 1
+    result = 0
+
     for l in lines:
-        pass
+        if l == "noop":
+            time += 1
+        else:
+            amount = int(l.split()[1])
+            time += 2
+            if (time - 20) % 40 == 1:
+                result += (time - 1) * reg
+
+            reg += amount
+        if (time - 20) % 40 == 0:
+            result += time * reg
 
     print(f"Result: {result}")
 
--- 2022/10/2.py
+++ 2022/10/2.py
@@ -1,17 +1,45 @@
 from timeit import default_timer as timer
 
 data_file = "sample.txt"
-# data_file = "data.txt"
+data_file = "data.txt"
+
+
+def render_image(screen: list) -> None:
+    for row in screen:
+        print("".join(row))
+
 
 start = timer()
 with open(data_file) as f:
     lines = f.read().splitlines()
 
-    result = None
-    for l in lines:
-        pass
+    screen_width = 40
+    screen_height = 6
 
-    print(f"Result: {result}")
+    time = 0
+    reg = 1
+    crt_x = 0
+    crt_y = 0
+    screen = [["." for _ in range(screen_width)] for _ in range(screen_height)]
+
+    for l in lines:
+        crt_x = time % 40
+        crt_y = time // 40
+
+        if crt_x in range(reg - 1, reg + 2):
+            screen[crt_y][crt_x] = "#"
+        time += 1
+
+        if l != "noop":
+            crt_x = time % 40
+            crt_y = time // 40
+            amount = int(l.split()[1])
+            if crt_x in range(reg - 1, reg + 2):
+                screen[crt_y][crt_x] = "#"
+            time += 1
+            reg += amount
+
+    render_image(screen)
 
 end = timer()
 print(f"Total time: {end - start:.5f} seconds.")
