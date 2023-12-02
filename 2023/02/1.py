52b17be25b757a7c10964cef5195ab5bdf7739e1
nuncaeslupus <imarcos@gmail.com>
2023-12-02 12:02:36 +0100

    Solve 2023-02

--- 2023/02/1.py
+++ 2023/02/1.py
@@ -1,15 +1,31 @@
 from timeit import default_timer as timer
 
 data_file = "sample.txt"
-# data_file = "data.txt"
+data_file = "data.txt"
+
+max_cubes = {"red": 12, "green": 13, "blue": 14}
 
 start = timer()
 with open(data_file, encoding="utf-8") as f:
+    possible_games: list[int] = []
     result = None
     for l in f:
-        pass
+        possible = True
+        game_num, games = l.split(":")
+        game_num = game_num.split()[1]
+        for game in games.split(";"):
+            for cubes in game.split(","):
+                num_cubes, color = cubes.split()
+                if max_cubes[color] < int(num_cubes):
+                    possible = False
+                    continue
+            if not possible:
+                continue
+        if not possible:
+            continue
+        possible_games.append(int(game_num))
 
-    print(f"Result: {result}")
+    print(f"Result: {sum(possible_games)}")
 
 end = timer()
 print(f"Total time: {end - start:.5f} seconds.")
--- 2023/02/2.py
+++ 2023/02/2.py
@@ -1,15 +1,28 @@
+from math import prod
 from timeit import default_timer as timer
 
 data_file = "sample.txt"
-# data_file = "data.txt"
+data_file = "data.txt"
+
+cube_order = {"red": 0, "green": 1, "blue": 2}
 
 start = timer()
 with open(data_file, encoding="utf-8") as f:
-    result = None
+    min_cubes: list[list[int]] = []
+    result: list[int] = []
     for l in f:
-        pass
+        current_game_min = [0, 0, 0]
+        game_num, games = l.split(":")
+        game_num = game_num.split()[1]
+        for game in games.split(";"):
+            for cubes in game.split(","):
+                num_cubes, color = cubes.split()
+                current_game_min[cube_order[color]] = max(
+                    current_game_min[cube_order[color]], int(num_cubes)
+                )
+        result.append(prod(current_game_min))
 
-    print(f"Result: {result}")
+    print(f"Result: {sum(result)}")
 
 end = timer()
 print(f"Total time: {end - start:.5f} seconds.")
