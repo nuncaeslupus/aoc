6d7cd6234cad0ab5d227a4e5539427848665f370
nuncaeslupus <imarcos@gmail.com>
2023-12-04 11:58:36 +0100

    Solve 2023-04

--- 2023/04/1.py
+++ 2023/04/1.py
@@ -1,15 +1,31 @@
 from timeit import default_timer as timer
 
 data_file = "sample.txt"
-# data_file = "data.txt"
+data_file = "data.txt"
 
 start = timer()
+table: list[tuple[set[int], set[int]]] = []
 with open(data_file, encoding="utf-8") as f:
     result = None
-    for l in f:
-        pass
+    for l in f.read().splitlines():
+        winning: set[int] = set()
+        have: set[int] = set()
+        groups = l.split("|")
+        winning_list = groups[0].split(":")[1]
+        winning = set([int(number) for number in winning_list.split()])
+        have = set([int(number) for number in groups[1].split()])
+        table.append((winning, have))
 
-    print(f"Result: {result}")
+points: list[int] = []
+for row in table:
+    matches = len(row[0].intersection(row[1]))
+    if matches:
+        points.append(2 ** (matches - 1))
+    else:
+        points.append(0)
+
+result = sum(points)
+print(f"Result: {result}")
 
 end = timer()
 print(f"Total time: {end - start:.5f} seconds.")
