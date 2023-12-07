11282a93d8952182dc1941e928b0d6dc3ec01413
nuncaeslupus <imarcos@gmail.com>
2023-12-07 19:09:14 +0100

    Solve 2023-06

--- 2023/06/1.py
+++ 2023/06/1.py
@@ -1,15 +1,30 @@
+from math import ceil, prod
 from timeit import default_timer as timer
 
-data_file = "sample.txt"
-# data_file = "data.txt"
 
+data_file = "sample.txt"
+data_file = "data.txt"
+
+races: list[tuple[int, int]] = []
+results: list[int] = []
 start = timer()
 with open(data_file, encoding="utf-8") as f:
     result = None
-    for l in f.read().splitlines():
-        pass
+    lines = f.read().splitlines()
+    times = lines[0].split(":")[1].split()
+    distances = lines[1].split(":")[1].split()
+    races = [(int(pair[0]), int(pair[1])) for pair in zip(times, distances)]
 
-    print(f"Result: {result}")
+for race in races:
+    count = 0
+    for i in range(ceil(race[0] / 2)):
+        if (race[0] - i) * i > race[1]:
+            count += 2
+    if race[0] % 2 == 0:
+        count += 1
+    results.append(count)
+
+print(f"Result: {prod(results)}")
 
 end = timer()
 print(f"Total time: {end - start:.5f} seconds.")
--- 2023/06/2.py
+++ 2023/06/2.py
@@ -1,15 +1,29 @@
+from math import ceil, prod
 from timeit import default_timer as timer
 
-data_file = "sample.txt"
-# data_file = "data.txt"
 
+data_file = "sample.txt"
+data_file = "data.txt"
+
+races: list[tuple[int, int]] = []
+results: list[int] = []
 start = timer()
 with open(data_file, encoding="utf-8") as f:
     result = None
-    for l in f.read().splitlines():
-        pass
+    lines = f.read().splitlines()
+    times = "".join(lines[0].split(":")[1].split())
+    distances = "".join(lines[1].split(":")[1].split())
+    race = (int(times), int(distances))
 
-    print(f"Result: {result}")
+count = 0
+for i in range(ceil(race[0] / 2)):
+    if (race[0] - i) * i > race[1]:
+        count += 2
+if race[0] % 2 == 0:
+    count += 1
+results.append(count)
+
+print(f"Result: {prod(results)}")
 
 end = timer()
 print(f"Total time: {end - start:.5f} seconds.")
