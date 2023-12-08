b86ffd3dfff72beef85ba20512844821039c524f
nuncaeslupus <imarcos@gmail.com>
2023-12-08 15:00:08 +0100

    Solve 2023-08

--- 2023/08/1.py
+++ 2023/08/1.py
@@ -1,15 +1,30 @@
 from timeit import default_timer as timer
 
-data_file = "sample.txt"
-# data_file = "data.txt"
+data_file = "sample2.txt"
+data_file = "data.txt"
 
 start = timer()
+mapping: dict[str, tuple[str, str]] = {}
 with open(data_file, encoding="utf-8") as f:
     result = None
-    for l in f.read().splitlines():
-        pass
+    lines = f.read().splitlines()
+    instructions = lines[0].replace("L", "0").replace("R", "1")
+    for l in lines[2:]:
+        l = l.replace("(", "").replace(")", "")
+        key, ways = l.split(" = ")
+        left, right = ways.split(", ")
+        mapping[key] = (left, right)
 
-    print(f"Result: {result}")
+result = 0
+pos = 0
+key = "AAA"
+while key != "ZZZ":
+    key = mapping[key][int(instructions[pos])]
+    result += 1
+    pos = (pos + 1) % len(instructions)
+
+
+print(f"Result: {result}")
 
 end = timer()
 print(f"Total time: {end - start:.5f} seconds.")
--- 2023/08/2.py
+++ 2023/08/2.py
@@ -1,15 +1,41 @@
 from timeit import default_timer as timer
+from math import lcm
 
-data_file = "sample.txt"
-# data_file = "data.txt"
+data_file = "sample3.txt"
+data_file = "data.txt"
 
 start = timer()
+mapping: dict[str, tuple[str, str]] = {}
 with open(data_file, encoding="utf-8") as f:
     result = None
-    for l in f.read().splitlines():
-        pass
+    lines = f.read().splitlines()
+    instructions = lines[0].replace("L", "0").replace("R", "1")
+    for l in lines[2:]:
+        l = l.replace("(", "").replace(")", "")
+        key, ways = l.split(" = ")
+        left, right = ways.split(", ")
+        mapping[key] = (left, right)
 
-    print(f"Result: {result}")
+
+result = 0
+startings = [key for key in mapping.keys() if key[-1] == "A"]
+ends = [key for key in mapping.keys() if key[-1] == "Z"]
+startends = {key: {} for key in startings}
+
+for starting in startings:
+    pos = 0
+    count = 1
+    key = starting
+    while key not in startends[starting]:
+        key = mapping[key][int(instructions[pos])]
+        pos = (pos + 1) % len(instructions)
+        if key[-1] == "Z":
+            startends[starting][key] = count
+        count += 1
+
+results = [[val for val in value.values()] for value in startends.values()]
+result = lcm(*list(item[0] for item in results))
+print(f"Result: {result}")
 
 end = timer()
 print(f"Total time: {end - start:.5f} seconds.")
